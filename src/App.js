0133f2bcbe3c93e0fb8fd9599e3d731008b678e6
Cleo1115 <chenyul5@illinois.edu>
2024-05-07 23:14:14 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { getContractNFTs } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={() => getContractNFTs("0x6811f2f20c42f42656A3c8623aD5e9461b83f719")}>click me</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
